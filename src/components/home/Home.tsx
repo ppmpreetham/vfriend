179b5f2e502dce2440fe5a2295e536ab57539106
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-06-12 15:21:34 +0530

    fix: scrollbar issues

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,7 +2,64 @@ import FriendCard from "./friendCard";
 
 const Home = () => {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full overflow-y-auto scrollbar-hide">
+      <FriendCard
+        available={true}
+        name="Preetham"
+        location="AB1-209"
+        time="11:30PM"
+      />
+      <FriendCard
+        available={false}
+        name="Sreeyansh"
+        location="AB3-110"
+        distance="30"
+        time="12:30 PM"
+      />
+      <FriendCard
+        available={false}
+        name="Raghav"
+        location="AB3-110"
+        time="12:30 PM"
+      />
+      <FriendCard
+        available={true}
+        name="Preetham"
+        location="AB1-209"
+        time="11:30PM"
+      />
+      <FriendCard
+        available={false}
+        name="Sreeyansh"
+        location="AB3-110"
+        distance="30"
+        time="12:30 PM"
+      />
+      <FriendCard
+        available={false}
+        name="Raghav"
+        location="AB3-110"
+        time="12:30 PM"
+      />
+      <FriendCard
+        available={true}
+        name="Preetham"
+        location="AB1-209"
+        time="11:30PM"
+      />
+      <FriendCard
+        available={false}
+        name="Sreeyansh"
+        location="AB3-110"
+        distance="30"
+        time="12:30 PM"
+      />
+      <FriendCard
+        available={false}
+        name="Raghav"
+        location="AB3-110"
+        time="12:30 PM"
+      />
       <FriendCard
         available={true}
         name="Preetham"
