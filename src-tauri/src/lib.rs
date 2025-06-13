10b570934bf9d70206543380577d7e43751f7485
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-06-14 01:42:37 +0530

    fix: qr code issue.
    thanks to @Lillianne from discord

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,17 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 pub mod commands;
 mod scheduling_conflict;
+// use tauri_plugin_barcode_scanner::init;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
     
-        // .plugin(tauri_plugin_barcode_scanner::init())
+        .setup(|app| {
+            #[cfg(mobile)]
+            app.handle().plugin(tauri_plugin_barcode_scanner::init());
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             commands::check_conflicts,
