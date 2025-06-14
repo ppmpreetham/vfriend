c1e27047a6ff427c77e886029512918b22e28397
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-06-14 10:20:25 +0530

    add: store plugin for storage

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,11 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 pub mod commands;
 mod scheduling_conflict;
-// use tauri_plugin_barcode_scanner::init;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-    
+        .plugin(tauri_plugin_store::Builder::new().build())
         .setup(|app| {
             #[cfg(mobile)]
             app.handle().plugin(tauri_plugin_barcode_scanner::init());
