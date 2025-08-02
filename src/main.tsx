f09c57339bb1d08337463cdc3a788d28d4b0db8f
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-08-02 11:43:45 +0530

    add: routing for about page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
 import { render } from "preact";
 import "./index.css";
 import App from "./app.tsx";
+import About from "./about/page.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-render(<App />, document.getElementById("app")!);
+const paths = [
+  { path: "/", component: App },
+  { path: "/about", component: About },
+] as const;
+
+const Main = () => (
+  <Router>
+    <Routes>
+      {paths.map(({ path, component: Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+    </Routes>
+  </Router>
+);
+render(<Main />, document.getElementById("app")!);
